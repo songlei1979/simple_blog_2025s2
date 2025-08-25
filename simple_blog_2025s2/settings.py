1c63512e41bc8cd896eb8d72739bc9915004621f
Lei <gabriel_sl19798@hotmail.com>
2025-08-25 19:25:37 +1200

    allowed all hosts

--- simple_blog_2025s2/settings.py
+++ simple_blog_2025s2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^0pi$xjh5&(ux5yq*^c)sc_8ym05qg*@pv_v2tok_mq_k4i2-d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
