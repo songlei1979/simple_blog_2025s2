1ba642e1ecfe2c4bcd56dd87b0813e2ae83f7e4e
Lei <gabriel_sl19798@hotmail.com>
2025-09-01 19:48:39 +1200

    namespace, bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index, post_detail
 
 urlpatterns = [
     path("", index, name="home"),
+    path("post/<int:pk>", post_detail, name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    posts = Post.objects.all()
+    return render(request,
+                  "index.html",
+                  {"posts": posts},)
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request,
+                  "post_detail.html",
+                  {"post": post})
